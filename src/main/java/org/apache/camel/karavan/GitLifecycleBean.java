5399452e241e9b1c17b3d38f22d2e493531a71cb
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-08-30 17:26:28 -0400

    Fix actions

---
+++ src/main/java/org/apache/camel/karavan/GitLifecycleBean.java
@@ -0,0 +1,13 @@
+package org.apache.camel;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+
+import io.quarkus.runtime.ShutdownEvent;
+import io.quarkus.runtime.StartupEvent;
+import org.jboss.logging.Logger;
+
+@ApplicationScoped
+
+public class GitLifecycleBean {
+}
