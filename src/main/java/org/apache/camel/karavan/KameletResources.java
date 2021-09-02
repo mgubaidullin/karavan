9a1f914533f56cba69f45b4212d8edf5d6abdd7c
Marat Gubaidullin <marat.gubaidullin@gmail.com>
2021-09-02 14:48:28 -0400

    Kamelet resources backend

--- src/main/java/org/apache/camel/karavan/KameletResources.java
+++ src/main/java/org/apache/camel/karavan/KameletResources.java
@@ -1,43 +1,14 @@
 package org.apache.camel.karavan;
 
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- * 
- *   http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
-
-import io.quarkus.runtime.StartupEvent;
-import io.vertx.core.json.JsonArray;
 import org.apache.camel.kamelets.catalog.KameletsCatalog;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.yaml.snakeyaml.Yaml;
-
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
 
